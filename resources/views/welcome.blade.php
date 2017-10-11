450afc69b17c9e224cb17019a49a7d23b829e6f7
suru <oooaa0727@gmail.com>
2017-10-11 11:54:42 +0800

    create link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
+
         <!-- Styles -->
         <style>
             html, body {
@@ -66,6 +67,7 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
+
             @if (Route::has('login'))
                 <div class="top-right links">
                     @if (Auth::check())
@@ -81,8 +83,11 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
+                <div>
+                    <a href="{{route('hello.index')}}">Hello</a>
+                </div>
                 <div class="links">
+
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
