f6fbd677716d6d6b977e45f4e01aa97dffe6a596
SU-RU <oooaa0727@gmail.com>
2017-11-19 12:48:02 +0800

    alter view：nav.blade,確認所有 href 設定都更改為對應的 route名稱

--- resources/views/posts/postsLayouts/partials/navigation.blade.php
+++ resources/views/posts/postsLayouts/partials/navigation.blade.php
@@ -4,25 +4,25 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-light bg-faded py-lg-4">
     <div class="container">
-        <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">Start Bootstrap</a>
+        <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="https://blackrockdigital.github.io/startbootstrap-business-casual/">Start Bootstrap</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav mx-auto">
+        <div class="collapse navbar-collapse"  id="navbarResponsive">
+            <ul class="navbar-nav mx-auto" align="center">
                 <li class="nav-item active px-lg-4">
-                    <a class="nav-link text-uppercase text-expanded" href="{{route('index')}}">Home
+                    <a class="nav-link text-uppercase text-expanded" href="{{route('posts.index')}}">Home
                         <span class="sr-only">(current)</span>
                     </a>
                 </li>
                 <li class="nav-item px-lg-4">
-                    <a class="nav-link text-uppercase text-expanded" href="{{route('about')}}">About</a>
+                    <a class="nav-link text-uppercase text-expanded" href="{{route('posts.about')}}">About</a>
                 </li>
                 <li class="nav-item px-lg-4">
-                    <a class="nav-link text-uppercase text-expanded" href="{{route('blog')}}">Blog</a>
+                    <a class="nav-link text-uppercase text-expanded" href="{{route('posts.blog')}}">Blog</a>
                 </li>
                 <li class="nav-item px-lg-4">
-                    <a class="nav-link text-uppercase text-expanded" href="{{route('contact')}}">Contact</a>
+                    <a class="nav-link text-uppercase text-expanded" href="{{route('posts.contact')}}">Contact</a>
                 </li>
             </ul>
         </div>
