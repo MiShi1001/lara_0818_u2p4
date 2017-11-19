7867a08c7b34bb64a22394d192fdb81c89f1025e
SU-RU <oooaa0727@gmail.com>
2017-11-19 12:44:23 +0800

    alter view，使之套用主樣板

--- resources/views/posts/blog.blade.php
+++ resources/views/posts/blog.blade.php
@@ -1,61 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
 @extends('posts.postsLayouts.master')
-<head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-@section('title','blog')
-
-<!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom fonts for this template -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
-
-    <!-- Custom styles for this template -->
-    <link href="css/business-casual.css" rel="stylesheet">
-
-</head>
-
-<body>
-
-<div class="tagline-upper text-center text-heading text-shadow text-white mt-5 d-none d-lg-block">Business Casual</div>
-<div class="tagline-lower text-center text-expanded text-shadow text-uppercase text-white mb-5 d-none d-lg-block">3481 Melrose Place | Beverly Hills, CA 90210 | 123.456.7890</div>
-
-
-<!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-light bg-faded py-lg-4">
-    <div class="container">
-        <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">Start Bootstrap</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav mx-auto">
-                <li class="nav-item active px-lg-4">
-                    <a class="nav-link text-uppercase text-expanded" href="{{route('index')}}">Home
-                        <span class="sr-only">(current)</span>
-                    </a>
-                </li>
-                <li class="nav-item px-lg-4">
-                    <a class="nav-link text-uppercase text-expanded" href="{{route('about')}}">About</a>
-                </li>
-                <li class="nav-item px-lg-4">
-                    <a class="nav-link text-uppercase text-expanded" href="{{route('blog')}}">Blog</a>
-                </li>
-                <li class="nav-item px-lg-4">
-                    <a class="nav-link text-uppercase text-expanded" href="{{route('contact')}}">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
+@section('title','Blog')
+@section('content')
 <div class="container">
 
     <div class="bg-faded p-4 my-4">
@@ -107,18 +52,4 @@
     </div>
 
 </div>
-<!-- /.container -->
-
-<footer class="bg-faded text-center py-5">
-    <div class="container">
-        <p class="m-0">Copyright &copy; Your Website 2017</p>
-    </div>
-</footer>
-<!-- Bootstrap core JavaScript -->
-<script src="js/jquery.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-
-</body>
-
-</html>
+@endsection
